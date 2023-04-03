2610a2a84a29c59d6eff6a82dc17302552523eee
huyhuyhuy112233 <nguyenanhhuy2004@gmail.com>
2023-04-03 18:05:39 +0700

    init_project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
+// import các thư viện react
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Demo from './computnent/Demo';
 
+// Nội dung toàn bộ website sẽ được chứa trong hàm render
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <div>
+  <Demo></Demo>
+  <Demo></Demo>
+  <Demo></Demo>
+  <Demo></Demo>
+ </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
